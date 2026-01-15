2785462efabe6a1c27f5a81c7efceace057c8cd4
Phatuurr <muhamad.fatur.rahaman@gmail.com>
2026-01-15 19:19:18 +0700

    save7

--- Tubes/resource/views/layout/footer.php
+++ Tubes/resource/views/layout/footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="container text-center">
         <img src="../../public/img/img1/logo footer.png" alt="Logo TaniMaju" class="footer-logo" onerror="this.src='https://cdn-icons-png.flaticon.com/512/628/628283.png'"
-        style="width: 50px;">
+        style="width: 90px; height: 30px;">
         <p class="opacity-75 mb-4">Mendorong Kedaulatan Pangan Indonesia melalui Informasi dan Teknologi.</p>
         <div class="d-flex justify-content-center gap-3 mb-4">
             <a href="#" class="text-white fs-5"><i class="bi bi-instagram"></i></a>
