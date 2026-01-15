a5fa7479f1ba69b9b01c51973b798dd6f9d40fcc
Alam <aaallaaamm03@gmail.com>
2026-01-15 22:37:49 +0700

    Update Lagi

--- Tubes/resource/views/register.php
+++ Tubes/resource/views/register.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-require_once '../../config/koneksi.php';
+require_once __DIR__ . '/../../config/koneksi.php';
+
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 $register_status = null;
